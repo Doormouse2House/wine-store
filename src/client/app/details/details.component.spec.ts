2bf40a92f18b82603255e8cd3ccbf6ccd929e898
Graham <grahamrmoss@gmail.com>
2018-03-04 16:26:33 +0000

    WP-9 Test Details onUpdate event

--- src/client/app/details/details.component.spec.ts
+++ src/client/app/details/details.component.spec.ts
@@ -4,6 +4,7 @@ import {
 } from '@angular/core/testing';
 
 import { DetailsComponent } from './details.component';
+import { ProducerModule } from '../producer/producer.module';
 
 export function main() {
   describe('Details component', () => {
@@ -26,6 +27,36 @@ export function main() {
             const compiled = fixture.nativeElement;
 
             expect(compiled).toBeTruthy();
+          });
+      })
+    );
+
+    it('should emit a producer and field id onUpdate',
+      async(() => {
+        TestBed
+          .compileComponents()
+          .then(() => {
+            const fixture = TestBed.createComponent(DetailsComponent);
+            const detailsInstance = fixture.componentInstance;
+            const expectedProducer = new ProducerModule();
+            const expectedId = 'myField';
+            const expectedValue = 'myValue';
+            const event = {
+              target: {
+                id: expectedId,
+                value: expectedValue
+              }
+            };
+
+            detailsInstance.producer = expectedProducer;
+
+            detailsInstance.producerVariableChange.subscribe((emission: any) => {
+              expect(emission.producer).toEqual(expectedProducer);
+              expect(emission.field).toEqual(expectedId);
+              expect(emission.producer[emission.field]).toEqual(expectedValue);
+            });
+
+            detailsInstance.onUpdate(event);
           });
       })
     );
